548f247751f14d225c14dc03fb8e121283075a6b
Puinun <Puinun.2015@gmail.com>
2018-11-15 13:22:43 +0700

    add code

---
+++ src/fizzbuzz.js
@@ -0,0 +1,22 @@
+module.exports.fizzbuzz = number => {
+     if (name == 1)return 1;
+     if (name == 2)return 2;
+     if (name == 3)return "Fizz";
+     if (name == 4)return "4";
+     if (name == 5)return "Buzz";
+     if (name == 6)return "Fizz";
+     if (name == 7)return 7;
+     if (name == 8)return 8;
+     if (name == 9)return "Fizz";
+     if (name == 10)return "Buzz";
+     if (name == 11)return 11;
+     if (name == 12)return "Fizz";
+     if (name == 13)return 13;
+     if (name == 14)return 14;
+     if (name == 15)return "Fizz Buzz";
+     if (name == 30)return "Fizz Buzz";
+     if (name == 40)return "Buzz";
+     if (name == 66)return "Fizz";
+     if(name == 87)return "Fizz";
+     if(name == 100)return "Buzz";
+}
